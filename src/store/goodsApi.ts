10b5f93bd3751344c4794dfb984a067dc45e68ba
Raman Krytski <rkeydevelop@gmail.com>
2023-01-18 20:43:18 +0100

    fix: export api

--- src/store/goodsApi.ts
+++ src/store/goodsApi.ts
@@ -1,6 +1,6 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 
-const goodsApi = createApi({
+export const goodsApi = createApi({
     reducerPath: 'goodsApi',
     baseQuery: fetchBaseQuery({
         baseUrl: 'http://localhost:5000/',
